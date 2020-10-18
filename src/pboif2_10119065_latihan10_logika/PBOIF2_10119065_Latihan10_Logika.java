9715b9d4cce9536936c07f4d6b15f80809c6730c
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-18 15:03:31 +0700

    commit files

---
+++ src/pboif2_10119065_latihan10_logika/PBOIF2_10119065_Latihan10_Logika.java
@@ -0,0 +1,19 @@
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini menampilkan hasil logika
+ *
+ */
+package pboif2_10119065_latihan10_logika;
+public class PBOIF2_10119065_Latihan10_Logika {
+    
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : "+ dewasa);
+    }
+    
+}
